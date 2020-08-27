b53ac2040427ef52350e068de58e8874ac5ec7aa
James Perrin <countrydawgg@gmail.com>
2020-08-27 16:02:59 -0700

    Completed video 161 - Fixing useAffect() warning.

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,19 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
-    // [] - Run at initial render
-    // ...nothing - Run at initial render AND Run after every rerender
-    // [data] - Run at initial render AND (Run after every rerender AND if data has changed)
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
+
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -16,27 +24,16 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
 
             setResults(data.query.search);
         };
 
-        if (term && !results.length) {
-            search();
-        } else {
-            const timeoutId = setTimeout(() => {
-                if (term) {
-                    search();
-                }
-            }, 750);
+        search();
 
-            return () => {
-                clearTimeout(timeoutId);
-            };
-        }
-    }, [term]);
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((results) => {
         return (
